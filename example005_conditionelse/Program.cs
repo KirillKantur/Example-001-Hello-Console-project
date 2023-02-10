b88b9eb19f798daa0426cacd2318e485e4daba95
Kirill Kantur <ludivopasnosti@gmail.com>
2023-02-10 19:31:35 +0300

    example005 alse

---
+++ example005_conditionelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Masha")
+{
+    Console.WriteLine("ura eto masha");
+}
+else
+{
+    Console.Write("privet");
+    Console.WriteLine(username);
+}
